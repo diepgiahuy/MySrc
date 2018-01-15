e3149b261b7e49571313e76b9259ef6eaa8fce06
diepgiahuy <30974088+diepgiahuy@users.noreply.github.com>
2018-01-15 10:21:31 +0700

    Update Source.cpp

--- OpenCvTest/Source.cpp
+++ OpenCvTest/Source.cpp
@@ -1,14 +1,61 @@
-#include < stdio.h>
-#include < iostream>
+﻿#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "MQTTClient.h"	
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
 #include <time.h>
-#include < opencv2\opencv.hpp>
-#include < opencv2/core/core.hpp>
-#include < opencv2/highgui/highgui.hpp>
-#include < opencv2/video/background_segm.hpp>
 
-
+#define ADDRESS     "tcp://localhost:1883"
+#define CLIENTID    "ExampleClientPub"
+#define TOPIC       "MQTT"
+//#define PAYLOAD     "Hello World"
+#define QOS         1
+#define TIMEOUT     10000L
 using namespace cv;
 using namespace std;
+
+MQTTClient_message pubmsg;
+MQTTClient client;
+int rc;
+void init()
+{
+	MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+	pubmsg = MQTTClient_message_initializer;
+
+	MQTTClient_create(&client, ADDRESS, CLIENTID,
+		MQTTCLIENT_PERSISTENCE_NONE, NULL);
+	conn_opts.keepAliveInterval = 20;
+	conn_opts.cleansession = 1;
+
+	if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+	{
+		printf("Failed to connect, return code %d\n", rc);
+		exit(-1);
+	}
+}
+
+void publish(char* PAYLOAD)
+{
+	pubmsg.payload = PAYLOAD;
+	pubmsg.payloadlen = strlen(PAYLOAD);
+	pubmsg.qos = QOS;
+	pubmsg.retained = 0;
+	MQTTClient_deliveryToken token;
+	MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+	printf("Waiting for up to %d seconds for publication of %s\n"
+		"on topic %s for client with ClientID: %s\n",
+		(int)(TIMEOUT / 1000), PAYLOAD, TOPIC, CLIENTID);
+	rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+	printf("Message with delivery token %d delivered\n", token);
+}
+
+void deinit()
+{
+	MQTTClient_disconnect(client, 10000);
+	MQTTClient_destroy(&client);
+}
+
 class Person {
 private:
 	long x, y, id;
@@ -45,44 +92,23 @@ int countPerson = 0;
 int enter = 0;
 int out = 0;
 static int id = 0;
-/** Global variables */
-String face_cascade_name = "\\home\\pi\\MQTT\\haarcascade_frontalface_alt.xml";
-String eyes_cascade_name = "\\home\\pi\\MQTT\\haarcascade_eye_tree_eyeglasses.xml";
-CascadeClassifier face_cascade;
-CascadeClassifier eyes_cascade;
-String window_name = "Capture - Face detection";
-Mat blur(Mat image)
-{
-	for (int i = 1; i<12; i = i + 2)
-	{
-		// smooth the image in the "src" and save it to "dst"
-		// blur(src, dst, Size(i,i));
 
-		// Gaussian smoothing
-		// GaussianBlur( src, dst, Size( i, i ), 0, 0 );
-
-		// Median smoothing
-		medianBlur(image, image, i);
-
-	
-	}
-	return image;
-}
-
-bool InActive(long x , long y , long curr)
+bool InActive(long x, long y, long curr)
 {
 	if (curr >= x && curr <= y)
 		return true;
 	return false;
 }
-void counting(Rect mr, long left , long right , long mid , long x , long y)
-{	
+void counting(Rect mr, long left, long right, long mid, long x, long y)
+{
+	bool isNew = true;
 	for (int i = 0; i < listPerson.size(); i++)
 	{
 		long def = sqrt(pow(x - listPerson[i].getX(), 2) + pow(y - listPerson[i].getY(), 2));
-		if (def <= x && def <= y)
+		if (def <= mr.width / 2 && def <= mr.height / 2)
 		{
-			if (InActive(left, right, listPerson[i].getX()))
+			isNew = false;
+			if (InActive(left, right, x))
 			{
 				if (x >= mid &&  listPerson[i].getX() < mid)
 				{
@@ -93,75 +119,50 @@ void counting(Rect mr, long left , long right , long mid , long x , long y)
 					countPerson--;
 				}
 				listPerson[i].update(x, y);
+				return;
+
 			}
 			else
 			{
 				listPerson.erase(listPerson.begin() + i);
+				return;
 			}
-			return;
 		}
 	}
+	if (isNew && InActive(left, right, x)) {
 		id++;
 		Person p = Person(x, y, id);
 		listPerson.push_back(p);
-
-}
-void detectAndDisplay(Mat frame)
-{
-	std::vector<Rect> faces;
-	Mat frame_gray;
-
-	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
-	equalizeHist(frame_gray, frame_gray);
-	//-- Detect faces
-	face_cascade.detectMultiScale(frame_gray, faces, 1.3, 5, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
-
-	for (size_t i = 0; i < faces.size(); i++)
-	{
-		Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
-		ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
-
-
 	}
-	//-- Show what you got
-	//imshow(window_name, frame);
+
 }
+
 
 int main()
 {
 
 	VideoCapture stream1(0);   //0 is the id of video device.0 if you have only one camera   
+	init();
 	Mat img1, img2, ContourImg;
 	Mat grayA, grayB, diffImage, binaryImg;
 	Mat morA;
 	stream1.read(img1);
 	cvtColor(img1, grayA, COLOR_BGR2GRAY);
 	cv::waitKey(1000);
-	img1 = blur(img1);
-	Mat element = getStructuringElement(MORPH_RECT, Size(7, 7), Point(3, 3));
 	long width = stream1.get(3);
-	long left = int(1* (width / 16));
+	long left = int(1 * (width / 16));
 	long right = int(4 * (width / 16));
 	long mid = int(2 * (width / 16));
 	long key = 0;
 	time_t start = time(0);
-	//-- 1. Load the cascades
-	if (!face_cascade.load(face_cascade_name)) { printf("--(!)Error loading face cascade\n"); return -1; };
-	if (!eyes_cascade.load(eyes_cascade_name)) { printf("--(!)Error loading eyes cascade\n"); return -1; };
-
+	int fcount = 0;
 	while (key != 'q') {
 
 		stream1.read(img2);
-		detectAndDisplay(img2);
 		cvtColor(img2, grayB, COLOR_BGR2GRAY);
 		absdiff(grayB, grayA, diffImage);
-		threshold(diffImage, binaryImg, 75, 255, CV_RETR_EXTERNAL);
-	//	morphologyEx(binaryImg, binaryImg, CV_MOP_ERODE, element);
-		morphologyEx(binaryImg, morA, CV_MOP_CLOSE, element);
-		//morphologyEx(morA, morA, CV_MOP_OPEN, element);
-
-		//morphologyEx(fgMaskMOG2, testImg, CV_MOP_OPEN, element);
-		ContourImg = morA.clone();
+		threshold(diffImage, binaryImg, 50, 255, CV_THRESH_BINARY);
+		ContourImg = binaryImg.clone();
 		vector< vector< Point> > contours;
 		findContours(ContourImg,
 			contours, // a vector of contours
@@ -171,18 +172,17 @@ int main()
 		line(img2, Point2i(right, 0), Point2i(right, 640), CV_RGB(255, 0, 0));
 		for (int i = 0; i < contours.size(); i++)
 		{
-			//if(contourArea(contours[i]) > 50000)
-			//cout << contourArea(contours[i]) << "\n";
-			if (contourArea(contours[i]) >  16000 )
+			if (contourArea(contours[i]) >  10000)
 			{
 				Rect mr = boundingRect(contours[i]);
-					Moments m = moments(contours[i]);
-					long x = int(m.m10 / m.m00);
-					long y = int(m.m01 / m.m00);
-					circle(img2, Point2i(x, y), 20, CV_RGB(255, 0, 0));
+				Moments m = moments(contours[i]);
+				long x = int(m.m10 / m.m00);
+				long y = int(m.m01 / m.m00);
+				circle(img2, Point2i(x, y), 20, CV_RGB(255, 0, 0));
 
-					rectangle(img2, mr, CV_RGB(255, 0, 0), 2);
-					counting(mr, left, right, mid, x, y);
+				//rectangle(img2, mr, CV_RGB(255, 0, 0), 2);
+				counting(mr, left, right, mid, x, y);
+
 			}
 		}
 		stringstream ss;
@@ -191,17 +191,19 @@ int main()
 		cv::putText(img2, NumberString.c_str(), Point(50, 100), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 200, 200), 4);
 		ss.clear();
 		double seconds_since_start = difftime(time(0), start);
-		if (seconds_since_start == 1)
+		if (seconds_since_start == 5)
 		{
 			start = time(0);
-			cout << seconds_since_start << " ";
-
+			if (fcount != countPerson)
+				publish((char*)&NumberString[0]);
+			fcount = countPerson;
 		}
 		cv::imshow("Diff", diffImage);
 		cv::imshow("Process", binaryImg);
 		cv::imshow("Ogrinal", grayA);
 		cv::imshow("NewImg", grayB);
 		cv::imshow("test", img2);
-		key = cv::waitKey(5);
+		key = cv::waitKey(50);
 	}
+	deinit();
 }
